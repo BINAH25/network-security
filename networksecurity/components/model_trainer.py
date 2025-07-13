4b2872f2252f968ce14442081e118ed35c4854e7
Louis Binah <louisbinah@Louiss-MacBook-Pro.local>
2025-07-13 21:14:49 +0000

    inference api added

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -49,15 +49,16 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
-    def track_mlflow(self, best_model, classificationmetric):
+    def track_mlflow(self, best_model, classificationmetric, stage: str):
         """
-        Logs F1, precision, recall scores to MLflow.
+        Logs model performance to MLflow for a specific stage (train/test).
         """
         with mlflow.start_run():
-            mlflow.log_metric("f1_score", classificationmetric.f1_score)
-            mlflow.log_metric("precision", classificationmetric.precision_score)
-            mlflow.log_metric("recall_score", classificationmetric.recall_score)
+            mlflow.log_metric(f"{stage}_f1_score", classificationmetric.f1_score)
+            mlflow.log_metric(f"{stage}_precision", classificationmetric.precision_score)
+            mlflow.log_metric(f"{stage}_recall", classificationmetric.recall_score)
             mlflow.sklearn.log_model(best_model, "model")
+
 
     def train_model(self, X_train, y_train, x_test, y_test):
         """
@@ -115,12 +116,15 @@ class ModelTrainer:
         # Train and evaluate on train data
         y_train_pred = best_model.predict(X_train)
         classification_train_metric = get_classification_score(y_true=y_train, y_pred=y_train_pred)
-        self.track_mlflow(best_model, classification_train_metric)
+        # Log train metrics
+        self.track_mlflow(best_model, classification_train_metric, stage="train")
+
 
         # Evaluate on test data
         y_test_pred = best_model.predict(x_test)
         classification_test_metric = get_classification_score(y_true=y_test, y_pred=y_test_pred)
-        self.track_mlflow(best_model, classification_test_metric)
+        # Log test metrics
+        self.track_mlflow(best_model, classification_test_metric, stage="test")
 
         # Load preprocessor
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
